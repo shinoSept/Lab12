00ae3114d4332e4eef88d884616c5eed491d6380
Chinoros <chinoros2815@gmail.com>
2024-12-24 14:37:58 +0700

    edited_2

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start) / CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
 }
 
-int fibonacci(int x) {
+long long int fibonacci(long long int x) {
     if (x == 0 || x == 1) {
         return x;
     }
